ce764233eca452d8d05c06ceffe86e2f2c4a6e06
Alik Welyn <alikwelynn@gmail.com>
2023-03-03 14:04:34 -0300

    added cards / added slider to cards / change arrows from slider

--- wp-content/themes/interlog-distribuicao/template-parts/trabalhe-conosco/vagas-abertas.php
+++ wp-content/themes/interlog-distribuicao/template-parts/trabalhe-conosco/vagas-abertas.php
@@ -4,16 +4,62 @@
             <div class="col-md-12">
                 <h4>Vagas atualmente abertas</h4>
             </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="periodo"><span>TEMPO INTEGRAL</span></div>
-                        <div class="local"><span>São Paulo</span></div>
+            <div class="col-md-12">
+                <div class="swiper-container vagas-abertas">
+                    <div class="swiper-wrapper">
+
+                    <div class="swiper-slide">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="periodo"><span>TEMPO INTEGRAL</span></div>
+                                <div class="local"><span>São Paulo</span></div>
+                            </div>
+                            <div class="card-body">
+                                <h5>Marketing 1</h5>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h5>Marketing</h5>
+
+                    <div class="swiper-slide">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="periodo"><span>TEMPO INTEGRAL</span></div>
+                                <div class="local"><span>São Paulo</span></div>
+                            </div>
+                            <div class="card-body">
+                                <h5>Marketing 2</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="periodo"><span>TEMPO INTEGRAL</span></div>
+                                <div class="local"><span>São Paulo</span></div>
+                            </div>
+                            <div class="card-body">
+                                <h5>Marketing 3</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="periodo"><span>TEMPO INTEGRAL</span></div>
+                                <div class="local"><span>São Paulo</span></div>
+                            </div>
+                            <div class="card-body">
+                                <h5>Marketing 4</h5>
+                            </div>
+                        </div>
+                    </div>
+                        
                     </div>
                 </div>
+                <div class="swiper-button-next" id="vagas-abertas-next"></div>
+                <div class="swiper-button-prev" id="vagas-abertas-prev"></div>
             </div>
         </div>
     </div>
