5dcea417c51ecb4cf5b308b39711939b5beb838a
Alik Welyn <alikwelynn@gmail.com>
2023-03-03 04:48:05 -0300

    added cf7 repeatable fields / edit plugin to make all this works more efficiently

--- wp-content/themes/interlog-distribuicao/footer.php
+++ wp-content/themes/interlog-distribuicao/footer.php
@@ -78,6 +78,7 @@ $(function() {
         allowSwipeToPrev: false
     });
 });
+
 </script>
 </body>
 </html>
--- wp-content/themes/interlog-distribuicao/functions/page-contato-plugin-repeater.php
+++ wp-content/themes/interlog-distribuicao/functions/page-contato-plugin-repeater.php
@@ -5,14 +5,16 @@ if (!defined('ABSPATH')) {
 
 function customize_add_button_atts( $attributes ) {
   return array_merge( $attributes, array(
-    'text' => 'ADICIONAR MAIS UM PRODUTO',
+    'additional_classes' => 'btn-default mb-5',
+    'text' => '<i class="fa fa-plus-circle" aria-hidden="true"></i> ADICIONAR PRODUTO',
   ) );
 }
 add_filter( 'wpcf7_field_group_add_button_atts', 'customize_add_button_atts' );
 
 function customize_remove_button_atts( $attributes ) {
   return array_merge( $attributes, array(
-    'text' => 'REMOVER PRODUTO',
+    'additional_classes' => 'btn-default mb-5',
+    'text' => '<i class="fa fa-minus-circle" aria-hidden="true"></i> REMOVER PRODUTO',
   ) );
 }
 add_filter( 'wpcf7_field_group_remove_button_atts', 'customize_remove_button_atts' );
