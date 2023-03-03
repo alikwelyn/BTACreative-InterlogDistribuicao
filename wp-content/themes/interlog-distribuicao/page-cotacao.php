8c8909b290b6d2048af75c532d67a863e73bdad2
Alik Welyn <alikwelynn@gmail.com>
2023-03-02 22:53:06 -0300

    added page cotacao

---
+++ wp-content/themes/interlog-distribuicao/page-cotacao.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+  <?php get_template_part('template-parts/shared/banner'); ?>
+  <?php get_template_part('template-parts/cotacao/form'); ?>
+<?php get_footer(); ?>
---
+++ wp-content/themes/interlog-distribuicao/template-parts/cotacao/form.php
@@ -0,0 +1,27 @@
+
+<section id="page-cotacao">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-cotacao">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-bs-toggle="pill" href="#cotacaoForm">Cotação</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="pill" href="#rastreamentoForm">Rastreamento</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane container active" id="cotacaoForm">
+                            <?php echo do_shortcode('[contact-form-7 id="14" title="Contact form 1"]'); ?>
+                        </div>
+                        <div class="tab-pane container fade" id="rastreamentoForm">
+                            Nothing
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
