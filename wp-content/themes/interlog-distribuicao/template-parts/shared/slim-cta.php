a1b6a515618ecbcb97f4f081e47d2e4b5597d948
Alik Welyn <alikwelynn@gmail.com>
2023-03-02 22:04:11 -0300

    added slim cta

---
+++ wp-content/themes/interlog-distribuicao/template-parts/shared/slim-cta.php
@@ -0,0 +1,10 @@
+<section id="slim-cta">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Interessado em um dos nossos serviços ou à procura de um serviço customizado?</h3>
+                <a href="#" class="btn-default">SOLICITE UMA COTAÇÃO</a>
+            </div>
+        </div>
+    </div>
+</section>
