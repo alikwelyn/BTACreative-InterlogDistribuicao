ccecde7faba2f17da7dbe40f320156f791d71f03
Alik Welyn <alikwelynn@gmail.com>
2023-03-03 13:26:48 -0300

    added junte-se a nossa equipe section

--- wp-content/themes/interlog-distribuicao/page-trabalhe-conosco.php
+++ wp-content/themes/interlog-distribuicao/page-trabalhe-conosco.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
   <?php get_template_part('template-parts/shared/banner'); ?>
+  <?php get_template_part('template-parts/trabalhe-conosco/junte-se-equipe'); ?>
 <?php get_footer(); ?>
---
+++ wp-content/themes/interlog-distribuicao/template-parts/trabalhe-conosco/junte-se-equipe.php
@@ -0,0 +1,12 @@
+<section id="junte-se-equipe">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <div class="content">
+                    <h4>Junte-se a nossa equipe</h4>
+                    <p>Para se candidatar a uma vaga, preencha o formulário abaixo e anexe seu currículo.<br>Arquivos permitidos: .doc .docx e .pdf</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
