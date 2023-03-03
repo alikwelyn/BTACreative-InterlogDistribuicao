d2678714d0799a8844e520d5e111662d9bc7b0db
Alik Welyn <alikwelynn@gmail.com>
2023-03-03 13:43:08 -0300

    added vagas abertas section

--- wp-content/themes/interlog-distribuicao/page-trabalhe-conosco.php
+++ wp-content/themes/interlog-distribuicao/page-trabalhe-conosco.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
   <?php get_template_part('template-parts/shared/banner'); ?>
   <?php get_template_part('template-parts/trabalhe-conosco/junte-se-equipe'); ?>
+  <?php get_template_part('template-parts/trabalhe-conosco/vagas-abertas'); ?>
 <?php get_footer(); ?>
---
+++ wp-content/themes/interlog-distribuicao/template-parts/trabalhe-conosco/vagas-abertas.php
@@ -0,0 +1,20 @@
+<section id="vagas-abertas">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Vagas atualmente abertas</h4>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="periodo"><span>TEMPO INTEGRAL</span></div>
+                        <div class="local"><span>São Paulo</span></div>
+                    </div>
+                    <div class="card-body">
+                        <h5>Marketing</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
