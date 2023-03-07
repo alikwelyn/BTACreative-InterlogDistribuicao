0fc8cb4a4f9cb4bec35a1312d4962b072a550887
Alik Welyn <alikwelynn@gmail.com>
2023-03-06 21:29:59 -0300

    added style for all pages

---
+++ wp-content/themes/interlog-distribuicao/page.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+  <?php get_template_part('template-parts/shared/banner'); ?>
+  <?php get_template_part('template-parts/shared/pages/index'); ?>
+<?php get_footer(); ?>
---
+++ wp-content/themes/interlog-distribuicao/template-parts/shared/pages/index.php
@@ -0,0 +1,11 @@
+<section id="op-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
