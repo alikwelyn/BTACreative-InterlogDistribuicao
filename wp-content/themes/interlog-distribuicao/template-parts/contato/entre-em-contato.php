9408688fe9a22b26318f73a418c7472acf3e372a
Alik Welyn <alikwelynn@gmail.com>
2023-03-08 20:12:26 -0300

    added aos to contato page

--- wp-content/themes/interlog-distribuicao/template-parts/contato/entre-em-contato.php
+++ wp-content/themes/interlog-distribuicao/template-parts/contato/entre-em-contato.php
@@ -1,7 +1,7 @@
 <section id="entre-em-contato">
     <div class="container">
         <div class="row">
-            <div class="col-md-8 mx-auto">
+            <div class="col-md-8 mx-auto" data-aos="fade-down">
                 <div class="content">
                     <?php echo get_field('contact_us_title', 'option'); ?>
                     <?php echo get_field('contact_us_text', 'option'); ?>
