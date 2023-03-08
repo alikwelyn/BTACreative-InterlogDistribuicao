b11fcd51cd5dad042d5eb3ad1f0bd2767c1b4413
Alik Welyn <alikwelynn@gmail.com>
2023-03-08 20:13:25 -0300

    added aos to all normal pages

--- wp-content/themes/interlog-distribuicao/template-parts/shared/pages/index.php
+++ wp-content/themes/interlog-distribuicao/template-parts/shared/pages/index.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="content">
+                <div class="content" data-aos="fade-up">
                     <?php the_content(); ?>
                 </div>
             </div>
